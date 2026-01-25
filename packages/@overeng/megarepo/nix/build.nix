ee605b7d73ed4a53d833eb76817626125bdeca0a
Johannes Schickling <schickling.j@gmail.com>
2026-01-25 13:04:28 +0100

    fix(megarepo): update pnpm deps hash again (CI environment differs)

--- packages/@overeng/megarepo/nix/build.nix
+++ packages/@overeng/megarepo/nix/build.nix
@@ -17,7 +17,7 @@ let
     # TODO: Re-enable typecheck once Effect lint warnings are resolved
     typecheck = false;
     depsManager = "pnpm";
-    pnpmDepsHash = "sha256-Cy1V7Aupr9YRugaeW7ScdRXVX5eo32vSYClVU8bZgqg=";
+    pnpmDepsHash = "sha256-SaLAfvzoWqSpPmNvHdcNl1f3KJiOif2ntip3IdaX8yo=";
     # Smoke test just runs --help which doesn't need git
     smokeTestArgs = [ "--help" ];
     dirty = dirty;
