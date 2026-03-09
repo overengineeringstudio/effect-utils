92955c6efa5c567554348737919ad367105f0183
Johannes Schickling <schickling.j@gmail.com>
2026-03-09 21:12:47 +0100

    Refresh oxlint-npm deps hash

--- nix/oxc-config-plugin.nix
+++ nix/oxc-config-plugin.nix
@@ -24,7 +24,7 @@ let
   lib = pkgs.lib;
   pnpmDepsHelper = import ./workspace-tools/lib/mk-pnpm-deps.nix { inherit pkgs; };
   packageDir = "packages/@overeng/oxc-config";
-  pnpmDepsHash = "sha256-/jDHaXYeMHPhMTrnGE7o6uvZA2BL16kVL9aDw0uzZMc=";
+  pnpmDepsHash = "sha256-gYT55AH7iYJYnQ2CItuP2k63t6NWDqW6SalpZ3cYKA0=";
 
   srcPath =
     if builtins.isAttrs src && builtins.hasAttr "outPath" src then
