b3b073c74816eccbbe8c71241f37595628244cd6
Johannes Schickling <schickling.j@gmail.com>
2026-04-18 22:38:52 +0200

    fix oxc-config dependency hash

--- nix/oxc-config-plugin.nix
+++ nix/oxc-config-plugin.nix
@@ -28,7 +28,7 @@ let
     pnpm = pinnedPnpm;
   };
   packageDir = "packages/@overeng/oxc-config";
-  pnpmDepsHash = "sha256-f2u4rJx+EpcBGOQQxeSzkcLXjKkBVYCvk2f3kXGQhOw=";
+  pnpmDepsHash = "sha256-wxdmM3lxotQwP01THMYW5Y61b27CSI1JA3VvCVCeN1c=";
 
   srcPath =
     if builtins.isAttrs src && builtins.hasAttr "outPath" src then
