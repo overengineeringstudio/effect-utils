0afb1c71dfd932221e43b01e3eaf1462a6366e79
schickling-assistant <261620128+schickling-assistant@users.noreply.github.com>
2026-07-01 17:25:43 +0200

    fix(nix): refresh notion pnpm dependency hashes

--- packages/@overeng/notion-cli/nix/build.nix
+++ packages/@overeng/notion-cli/nix/build.nix
@@ -40,7 +40,7 @@ let
     depsBuilds = {
       "." = {
         hash = selectHashForSystem {
-          aarch64-darwin = "sha256-/Fho7Vj38qsmunkllmVEaIHp0oIlYg01aVo3PMn1+yo=";
+          aarch64-darwin = "sha256-ouJfbBUII19nKhH7WC7GPr8/rM0RRTkcliMhSIVs53U=";
           aarch64-linux = "sha256-Ox+FChLtOlgYkNVZX7oJDuruiRYpElH+bOzBmsbn1uk=";
           x86_64-linux = "sha256-ouJfbBUII19nKhH7WC7GPr8/rM0RRTkcliMhSIVs53U=";
         };
