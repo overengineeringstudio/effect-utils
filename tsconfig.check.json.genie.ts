a284fa43d40d7235adc85428469b2a0b015739f2
schickling-assistant <261620128+schickling-assistant@users.noreply.github.com>
2026-07-19 16:04:29 +0200

    chore(genie): move tsconfig project registry

--- tsconfig.check.json.genie.ts
+++ tsconfig.check.json.genie.ts
@@ -1,6 +1,6 @@
 import type { TSConfigArgs } from './packages/@overeng/genie/src/runtime/mod.ts'
 import { tsconfigJson } from './packages/@overeng/genie/src/runtime/mod.ts'
-import { rootTsconfigProjects } from './tsconfig.projects.ts'
+import { rootTsconfigProjects } from './genie/tsconfig-projects.ts'
 
 // This file is meant for convenience to built all TS projects in the workspace at once
 export default tsconfigJson({
--- tsconfig.emit.json.genie.ts
+++ tsconfig.emit.json.genie.ts
@@ -1,6 +1,6 @@
 import { isTsconfigReferenceTarget } from './packages/@overeng/genie/src/runtime/composition/mod.ts'
 import { tsconfigJson, type TSConfigArgs } from './packages/@overeng/genie/src/runtime/mod.ts'
-import { rootTsconfigProjects } from './tsconfig.projects.ts'
+import { rootTsconfigProjects } from './genie/tsconfig-projects.ts'
 
 export default tsconfigJson({
   files: [],
